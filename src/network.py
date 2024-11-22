b9a32aa31af7e9aa8f2c2d0ba6cec342b1934676
Rodrigo-Flores <dev.rodrigoflores@gmail.com>
2024-11-21 23:23:57 -0300

    docs(documentation): :memo: include structured explanation for Network methods

--- src/network.py
+++ src/network.py
@@ -37,7 +37,7 @@ class Network:
 		"""
 		return 1.0 / (1.0 + np.exp(-z))
 	
-	def sigmoid_prime(self, z): 
+	def sigmoid_prime(self, z) -> np.ndarray: 
 		"""
 		The derivative of the sigmoid function.
 
@@ -77,14 +77,22 @@ class Network:
 			eta: float,
 			test_data: list[tuple] = None
 		) -> None:
-		"""Train the neural network using mini-batch stochastic
-		gradient descent.  The "training_data" is a list of tuples
-		"(x, y)" representing the training inputs and the desired
-		outputs.  The other non-optional parameters are
-		self-explanatory.  If "test_data" is provided then the
-		network will be evaluated against the test data after each
-		epoch, and partial progress printed out.  This is useful for
-		tracking progress, but slows things down substantially."""
+		"""
+		Stochastic Gradient Descent.
+	
+		Desc:
+			Train the neural network using mini-batch stochastic gradient descent.
+
+		Args:
+			training_data (list[tuple]): A list of tuples "(x, y)" representing the training inputs and the desired outputs.
+			epochs (int): The number of epochs to train for.
+			mini_batch_size (int): The size of each mini-batch.
+			eta (float): The learning rate.
+			test_data (list[tuple], optional): A list of tuples "(x, y)" representing the test inputs and the desired outputs.
+
+		Returns:
+			None
+		"""
 		if test_data: n_test = len(test_data)
 		n = len(training_data)
 		for j in range(epochs):
@@ -100,11 +108,20 @@ class Network:
 			else:
 				print("Epoch {0} complete".format(j))
 
-	def update_mini_batch(self, mini_batch, eta):
-		"""Update the network's weights and biases by applying
-		gradient descent using backpropagation to a single mini batch.
-		The "mini_batch" is a list of tuples "(x, y)", and "eta"
-		is the learning rate."""
+	def update_mini_batch(self, mini_batch, eta) -> None:
+		""".
+		Update mini batch.
+
+		Desc:
+			Update the network's weights and biases by applying gradient descent using backpropagation to a single mini batch
+
+		Args:
+			mini_batch (list[tuple]): A list of tuples "(x, y)" representing the training inputs and the desired outputs.
+			eta (float): The learning rate.
+
+		Returns:
+			None
+		"""
 		nabla_b = [np.zeros(b.shape) for b in self.biases]
 		nabla_w = [np.zeros(w.shape) for w in self.weights]
 		for x, y in mini_batch:
@@ -114,11 +131,20 @@ class Network:
 		self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
 		self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
 
-	def backprop(self, x, y):
-		"""Return a tuple ``(nabla_b, nabla_w)`` representing the
-		gradient for the cost function C_x.  ``nabla_b`` and
-		``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-		to ``self.biases`` and ``self.weights``."""
+	def backprop(self, x, y) -> tuple:
+		"""
+		Backpropagation.
+
+		Desc:
+			Feedforward and backward pass known as backpropagation. Changes the weights and biases of the network.
+
+		Args:
+			x (int): The input to the network.
+			y (int): The desired output of the network.
+
+		Returns:
+			tuple: A tuple containing the changes in the biases and weights of the network.
+		"""
 		nabla_b = [np.zeros(b.shape) for b in self.biases]
 		nabla_w = [np.zeros(w.shape) for w in self.weights]
 		# feedforward
@@ -148,15 +174,34 @@ class Network:
 			nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
 		return (nabla_b, nabla_w)
 	
-	def evaluate(self, test_data):
-		"""Return the number of test inputs for which the neural
-		network outputs the correct result. Note that the neural
-		network's output is assumed to be the index of whichever
-		neuron in the final layer has the highest activation."""
+	def evaluate(self, test_data) -> int:
+		"""
+		Evaluate.
+
+		Desc:
+			Return the number of test inputs for which the neural network outputs the correct result.
+
+		Args:
+			test_data (list[tuple]): A list of tuples "(x, y)" representing the test inputs and the desired outputs.
+
+		Returns:
+			int: The number of test inputs for which the neural network outputs the correct
+		"""
 		test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
 		return sum(int(x == y) for (x, y) in test_results)
 
-	def cost_derivative(self, output_activations, y):
-		"""Return the vector of partial derivatives \partial C_x /
-		\partial a for the output activations."""
+	def cost_derivative(self, output_activations, y) -> np.ndarray:
+		"""
+		Cost derivative.
+
+		Desc:
+			Return the vector of partial derivatives \partial C_x / \partial a for the output activations.
+
+		Args:
+			output_activations (int): The output of the network.
+			y (int): The desired output of the network.
+
+		Returns:
+			np.ndarray: The vector of partial derivatives.
+		"""
 		return (output_activations-y)
